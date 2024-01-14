eb831569372060df4d3082509aae509ba23d2204
Dejvid Manuka <devimanuka02@gmail.com>
2024-01-14 18:20:19 +0100

    putting it together change

--- propItUP/propItUp/src/assets/PersonCard.jsx
+++ propItUP/propItUp/src/assets/PersonCard.jsx
@@ -1,13 +1,15 @@
-import React from "react"
+import React,{useState} from "react"
 
 const Person = (props) =>{
-
+    const [state, setState] = useState(props.age);
+    
     return(
 
         <div>
             <h1>{props.name}</h1>
-            <h4>Age : {props.age}</h4>
+            <h4>Age : {state}</h4>
             <h4>Hair color : {props.hC}</h4>
+            <button onClick={ (event) => setState(state + 1)}>Birthday button for {props.name}</button>
         </div>
 
     );
